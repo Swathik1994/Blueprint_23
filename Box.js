9e6552f699d41f8448197097fdbd6a4f6d3c685d
Swathik1994 <70898286+Swathik1994@users.noreply.github.com>
2020-11-13 11:24:53 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Box{
+
+//properties
+
+constructor(x,y,width,height){
+
+    var options={
+        restitution: 1.0
+    }
+
+this.body=Bodies.rectangle(x,y,width,height,options);
+World.add(world,this.body);
+
+}
+//functions
+
+display(){
+
+    var pos=this.body.position;
+    rectMode(CENTER);
+    fill (255);
+    rect(pos.x,pos.y,this.width,this.height);
+
+}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blueprint_23
+# p5.play-boilerplate
+Boiler plate for p5.play
